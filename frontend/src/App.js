40f89733de4db4fcedc6ba2011eaf4b1778979c9
[2기-1반] 최연주 <jujubee6323@gmail.com>
2021-11-11 17:47:00 +0900

    [#5]chore: frontend 환경 구축 추가
    react-router-dom, styled-components 설치 및 라우팅 작업 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Main from './routes/Main';
+import Result from './routes/Result';
+import Search from './routes/Search'
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Main />} />
+          <Route exact path='/result/:id' element={<Result />} />
+          <Route exact path='/search' element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
