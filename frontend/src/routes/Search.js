bc565eb45a4ab397f9db56e97c7a66cbf71299c1
nahyunn <nhkim0711@gmail.com>
2021-11-12 17:05:24 +0900

    [#12]feat: 엠블럼 이미지 가져오기

--- frontend/src/routes/Search.js
+++ frontend/src/routes/Search.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import get_emblem from "../utils/get_emblem";
+
+const emblem_list = get_emblem().map((emblem) => {
+  console.log(emblem);
+  return <img key={`${emblem[0]}`} art={`${emblem[0]}`} src={`${emblem[1]}`} />;
+});
 
 export default function Search() {
-    return (
-        <div>
-            This is Search
-        </div>
-    )
+  return <div>{emblem_list}</div>;
 }
