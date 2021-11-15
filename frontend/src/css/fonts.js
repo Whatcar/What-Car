4d4b613183e1158bdfe0a62e0d229ef0bd6eee6f
nahyunn <nhkim0711@gmail.com>
2021-11-15 12:01:57 +0900

    [#18]feat: 컬러 폰트 상수 설정

---
+++ frontend/src/css/fonts.js
@@ -0,0 +1,31 @@
+import { css } from "styled-components";
+
+export const maintitle = css`
+  font-size: 48px;
+  font-weight: bold;
+  line-height: 64px;
+`;
+
+export const subtitle = css`
+  font-size: 32px;
+  font-weight: medium;
+  line-height: 48px;
+`;
+
+export const body = css`
+  font-size: 16px;
+  font-weight: light;
+  line-height: 30px;
+`;
+
+export const menu = css`
+  font-size: 24px;
+  font-weight: medium;
+  line-height: 40px;
+`;
+
+export const desc = css`
+  font-size: 12px;
+  font-weight: light;
+  line-height: 15px;
+`;
--- frontend/src/routes/Search.js
+++ frontend/src/routes/Search.js
@@ -1,11 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import Container from "@mui/material/Container";
+
 import get_emblem from "../utils/get_emblem";
 
 const emblem_list = get_emblem().map((emblem) => {
-  console.log(emblem);
   return <img key={`${emblem[0]}`} art={`${emblem[0]}`} src={`${emblem[1]}`} />;
 });
 
-export default function Search() {
-  return <div>{emblem_list}</div>;
-}
+const Search = () => {
+  return (
+    <Container>
+      hello!
+      <Box />
+    </Container>
+  );
+};
+
+export default Search;
+
+const Box = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: "red";
+`;
