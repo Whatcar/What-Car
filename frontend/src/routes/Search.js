bcbe3b94ab70e76a695e2bc12fbd067dec259b43
nahyunn <nhkim0711@gmail.com>
2021-11-16 10:50:55 +0900

    fix: 중복 그리드 삭제

--- frontend/src/routes/Search.js
+++ frontend/src/routes/Search.js
@@ -1,25 +1,17 @@
-import React from "react";
-import styled from "styled-components";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+import React from 'react';
+import styled from 'styled-components';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
-import SelectBox from "../components/SelectBox";
-import { maintitle } from "../css/fonts";
+import SelectBox from '../components/SelectBox';
+import { maintitle } from '../css/fonts';
 
 const Search = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={0}>
-        <Grid item xs={2} />
-        <Grid item xs={8}>
-          <ContentBox>
-            <Title>어떤 차가 궁금하신가요?</Title>
-            <SelectBox />
-          </ContentBox>
-        </Grid>
-        <Grid item xs={2} />
-      </Grid>
-    </Box>
+    <ContentBox>
+      <Title>어떤 차가 궁금하신가요?</Title>
+      <SelectBox />
+    </ContentBox>
   );
 };
 
