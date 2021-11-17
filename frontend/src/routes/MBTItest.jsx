83b6f904eb6f9a37056def503f516cfd86a516db
[2기-1반] 최연주 <jujubee6323@gmail.com>
2021-11-17 11:15:41 +0900

    [#25, #26, #28] MBTI, 팀페이지, 404 페이지 구현

--- frontend/src/routes/MBTItest.jsx
+++ frontend/src/routes/MBTItest.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LinearProgress, Button } from '@mui/material';
+import { blue } from '../css/colors.js';
+import styled from 'styled-components';
+import questions from '../data/mbtiQuestions.js';
+import Questions from '../components/MBTI/Questions.jsx';
 
 export default function MBTItest() {
-  return <div>This is test page</div>;
+  const [progress, setProgress] = useState(0);
+  // TODO: 페이지네이션, 버튼 연결, 계산 로직 구현
+  return (
+    <>
+      <ProgressBar>
+        <div style={{ textAlign: 'right' }}>{progress + 1}/9</div>
+        <LinearProgress variant="determinate" color="inherit" value={((progress + 1) * 100) / 9} />
+      </ProgressBar>
+      <Questions item={questions[progress]} progress={progress} />
+      <Buttons>
+        <Button variant="contained" size="large" sx={{ backgroundColor: blue.main }}>
+          이전
+        </Button>
+        <Button variant="contained" size="large" sx={{ backgroundColor: blue.main }}>
+          다음
+        </Button>
+      </Buttons>
+    </>
+  );
 }
+
+const ProgressBar = styled.div``;
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  button {
+    width: 200px;
+    margin-top: 2rem;
+  }
+`;
