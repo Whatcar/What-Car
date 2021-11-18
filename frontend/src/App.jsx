33087711f02c47c94ea15a61f0f44908d8b8077c
nahyunn <nhkim0711@gmail.com>
2021-11-18 15:11:40 +0900

    fix: keyName name -> range 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,25 +12,29 @@ import Worldcup from './routes/Worldcup.jsx';
 import Team from './routes/Team.jsx';
 import MBTItest from './routes/MBTItest.jsx';
 import NotFound from './routes/NotFound.jsx';
+import { ThemeProvider } from '@mui/material';
+import muiTheme from './css/muiTheme.js';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Contents>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/result/:id" element={<Result />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/mbti" element={<MBTI />} />
-            <Route path="/mbti/test" element={<MBTItest />} />
-            <Route path="/worldcup" element={<Worldcup />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </Contents>
-      </BrowserRouter>
+      <ThemeProvider theme={muiTheme}>
+        <BrowserRouter>
+          <Navbar />
+          <Contents>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/result/:id" element={<Result />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/mbti" element={<MBTI />} />
+              <Route path="/mbti/test" element={<MBTItest />} />
+              <Route path="/worldcup" element={<Worldcup />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </Contents>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
