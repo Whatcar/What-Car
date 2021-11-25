0c120df44e046d68959ae68b3203ffd501cb10f5
redhi <redji81@naver.com>
2021-11-25 15:56:20 +0900

    [#61]fix: 검색결과가 없을 시 빈배열 형태로 변경

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,15 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:비번@127.0.0.1:3306/디비명?charset=utf8mb4"
+aws_db = {
+    "user": "admin",
+    "password": "1q2w3e4r!",
+    "host": "database-1.cbmthuyhcguk.ap-northeast-2.rds.amazonaws.com",
+    "port": "3306",  # Maria DB의 포트
+    "database": "whatcar",
+}
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{aws_db['user']}:{aws_db['password']}@{aws_db['host']}:{aws_db['port']}/{aws_db['database']}?charset=utf8"
+print(SQLALCHEMY_DATABASE_URI)
 SQLALCHEMY_TRACK_MODIFICATIONS = 1
 secret_key = "asdasdasdasd"
