95775a5fc252bfcc1036cf98829091c4e9ab4084
redhi <redji81@naver.com>
2021-12-06 11:08:44 +0900

    [#96]fix: 4주차 코드리뷰 일부 반영
    Former-commit-id: 874143971c9266995597f7bdc969e1c268ea045f

--- backend/models/worldcup.py
+++ backend/models/worldcup.py
@@ -4,9 +4,10 @@ from flask_restx import fields
 
 # 월드컵
 class WorldCup(db.Model):
-    __tablename__ = "WORLD_CUP"
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    car_id = db.Column(db.Integer, db.ForeignKey("CAR.id"))
+    __tablename__ = "world_cup"
+    car_id = db.Column(
+        db.Integer, db.ForeignKey("car.id"), primary_key=True, nullable=False
+    )
     photolink = db.Column(db.Text, nullable=False)
     count = db.Column(db.Integer, nullable=False)
 
@@ -22,7 +23,7 @@ class WorldCup(db.Model):
     def to_dict(self):
         return {"car_id": self.car_id, "photolink": self.photolink}
 
-    wordlcup_model_result = {
+    response_model = {
         "car_id": fields.Integer(required=True, description="차량 고유 아이디"),
         "name": fields.String(required=True, description="차량 모델명"),
         "count": fields.Integer(required=True, description="우승한 횟수"),
@@ -32,7 +33,7 @@ class WorldCup(db.Model):
         "is_result": fields.Boolean(required=True, description="현재 우승 결과 여부"),
     }
 
-    worldcup_model_rand_result = {
+    response_model_rand = {
         "car_id": fields.Integer(required=True, description="차량 고유 아이디"),
         "photolink": fields.String(required=True, description="블러 처리된 차량 이미지 링크"),
     }
--- backend/service/upload_service.py
+++ backend/service/upload_service.py
@@ -1,15 +1,14 @@
 import os
 import random
 
-from ai import label, model
+# from ai import label, model
 from config import aws_s3
-from flask import request
 from s3_connection import s3_connection
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 
 
-def register(data):
+def get_upload_result(data):
     if not data:
         abort(404, "파일이 존재하지 않습니다.")
     folder_name = "./static/image/"
@@ -27,10 +26,10 @@ def register(data):
     #     ContentType=data.content_type,
     # )
     print(file_location)
-    predict_value = model.predict(file_location)
-    label_id = predict_value[0]
-    id = int(label.label_dict[label_id])
-    print(label.label_dict[label_id], label_id, predict_value[1])
+    # predict_value = model.predict(file_location)
+    # label_id = predict_value[0]
+    # id = int(label.label_dict[label_id])
+    # print(label.label_dict[label_id], label_id, predict_value[1])
     os.remove(file_location)
 
     return {"id": id}
