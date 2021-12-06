bee21daeb7ba923b5ef0917968aaaffb9671f3e3
redhi <redji81@naver.com>
2021-12-07 05:10:13 +0900

    [#103, #108]feature: 업로드 페이지 내 필요한 API 구현

--- backend/controller/upload_controller.py
+++ backend/controller/upload_controller.py
@@ -1,7 +1,6 @@
 from flask import request
 from flask_restx import Namespace, Resource, fields
-from service import (get_ai_cars_detail, get_same_id_gallery_img,
-                     get_upload_result)
+from service import get_ai_cars_detail, get_same_id_gallery_img, get_upload_result
 from werkzeug.datastructures import FileStorage
 
 upload = Namespace("upload", path="/api")
@@ -36,15 +35,15 @@ class Upload(Resource):
         return similarity_car_db_id, 200
 
 
-@upload.route("/upload/gallery", methods=["GET"])
-class Upload_Gallery(Resource):
-    @upload.doc(params={"id": "차량의 임시 DB 고유 아이디"})
-    @upload.response(200, "Success", result)
-    @upload.response(404, "해당 url이 만료되었습니다.")
-    def get(self):
-        """해당 자동차 분석 결과의 가장 유사 차량과 동일한 차량 갤러리 상세 정보를 가져옵니다."""
-        id = request.args.get("id")
-        num = request.args.get("num", type=int, default=1)
-        same_gallery_content = get_same_id_gallery_img(id, num)
+# @upload.route("/upload/gallery", methods=["GET"])
+# class Upload_Gallery(Resource):
+#     @upload.doc(params={"id": "차량의 고유 아이디"})
+#     @upload.response(200, "Success", result)
+#     @upload.response(404, "해당 url이 만료되었습니다.")
+#     def get(self):
+#         """해당 자동차 분석 결과의 가장 유사 차량과 동일한 차량 갤러리 상세 정보를 가져옵니다."""
+#         id = request.args.get("id")
+#         num = request.args.get("num", type=int, default=1)
+#         same_gallery_content = get_same_id_gallery_img(id, num)
 
-        return same_gallery_content
+#         return same_gallery_content
