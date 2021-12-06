e877cf5b4eeb1ba825cb3ab1ca4494c43b3d0aa1
redhi <redji81@naver.com>
2021-12-07 05:10:13 +0900

    [#103, #108]feature: 업로드 페이지 내 필요한 API 구현
    Former-commit-id: 79934c049a21cb5cb747cd40f44bd7b71c065268

--- backend/service/upload_service.py
+++ backend/service/upload_service.py
@@ -16,40 +16,6 @@ from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 
 
-def pagination(query_list, num):
-
-    query_data_num = query_list.count()
-    print(query_data_num)
-    per_page = 9
-    range_start = per_page * (num - 1)
-    total_page = query_data_num // per_page
-    rest_data = query_data_num % per_page
-
-    # # 검색 결과가 없으면
-    if query_data_num == 0:
-        car_list = []
-
-    # per_page가 다 채워지지 않은 마지막 페이지를 넘으면
-    elif num >= (total_page + 1) and rest_data == 0:
-        abort(404, "페이지 범위를 초과했습니다.")
-
-    # 전체 데이터 개수//페이지 당 데이터가 들어온 페이지 범위를 넘으면
-    elif num > (total_page + 1):
-        abort(404, "페이지 범위를 초과했습니다.")
-
-    # 전체 데이터 개수//페이지 당 데이터가 들어온 페이지 같으면
-    if num == (total_page + 1) and not rest_data == 0:
-        # 범위 시작값부터 마지막 데이터까지
-        query_list = query_list[range_start:query_data_num]
-    else:
-        query_list = query_list[range_start : per_page * num]
-
-    car_list = [Gallery.to_dict(car) for car in query_list]
-
-    result = {"result_num": query_data_num, "gallery_content": car_list}
-    return result
-
-
 def get_upload_result(data):
     if not data:
         abort(404, "파일이 존재하지 않습니다.")
@@ -120,7 +86,7 @@ def get_upload_result(data):
     print(id)
 
     os.remove("result.jpg")
-    return {"id": id}
+    return {"id": id, "car_id": result[0][0]}
 
 
 def get_ai_cars_detail(id):
@@ -180,13 +146,13 @@ def get_ai_cars_detail(id):
     return result
 
 
-def get_same_id_gallery_img(id, num):
-    data = Temporary_Ai_Car.query.filter(Temporary_Ai_Car.id == id).first()
+# def get_same_id_gallery_img(id, num):
+#     # data = Temporary_Ai_Car.query.filter(Temporary_Ai_Car.id == id).first()
 
-    content = Temporary_Ai_Car.to_dict(data)
-    most = content["most_similar_car"]
-    same_id_gallery = Gallery.query.filter(Gallery.car_id == most[0])
-    # same_id_gallery2 = [Gallery.to_dict(content) for content in same_id_gallery]
-    gallery_contents = pagination(same_id_gallery, num)
-    # print(same_id_gallery2)
-    return gallery_contents
+#     # content = Temporary_Ai_Car.to_dict(data)
+#     # most = content["most_similar_car"]
+#     same_id_gallery = Gallery.query.filter(Gallery.car_id == id)
+#     # same_id_gallery2 = [Gallery.to_dict(content) for content in same_id_gallery]
+#     gallery_contents = pagination(same_id_gallery, num)
+#     # print(same_id_gallery2)
+#     return gallery_contents
