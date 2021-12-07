38c92dfc8191e5142c3a92e9be64a948a2ae0912
JeongGod <jeonggu223@gmail.com>
2021-12-08 00:53:55 +0900

    [#105] feat: gallary API 수정
    Former-commit-id: bd88a3d95f4c645f5d0b95ec2cb6530dba0013be

--- backend/s3_connection.py
+++ backend/s3_connection.py
@@ -16,3 +16,18 @@ def s3_connection():
     else:
         print("s3 bucket connected!")
         return s3
+
+
+def s3_resource():
+    try:
+        s3 = boto3.resource(
+            "s3",
+            aws_access_key_id=aws_s3["AWS_ACCESS_KEY"],
+            aws_secret_access_key=aws_s3["AWS_SECRET_KEY"],
+        )
+    except Exception as e:
+        print(e)
+        exit(ERROR_S3_CONNECTION_FAILED)
+    else:
+        print("s3 bucket connected!")
+        return s3
--- backend/service/gallary_service.py
+++ backend/service/gallary_service.py
@@ -1,6 +1,8 @@
+from config import aws_s3
 from db_connect import db
 from flask import abort
-from models import Car, Gallary
+from models import Ai_Result, Car, Gallary
+from s3_connection import s3_resource
 from werkzeug.exceptions import HTTPException
 
 
@@ -31,21 +33,48 @@ def get_gallary_cars(off_set, limit_num):
 
 def post_gallary_cars(info):
     try:
+        ai_db_id = info["ai_db_id"]
         car_id = info["car_id"]
         car_url = info["car_url"]
         similarity = info["similarity"]
         nickname = info["nickname"]
         pw = info["password"]
 
+        # s3 bucket 영구저장
+        s3 = s3_resource()
+
+        ai_db = Ai_Result.query.filter_by(id=ai_db_id).first()
+
+        # 파일 복사
+        copy_key = "/".join(ai_db.most_similar_car_url.split("/")[3:])
+        copy_image = {"Bucket": "whatcar", "Key": copy_key}
+        src_folder = "upload"
+        dst_folder = "gallary"
+        new_key = copy_key.replace(src_folder, dst_folder)
+        s3_bucket = s3.Bucket("whatcar")
+        new_obj = s3_bucket.Object(new_key)
+        new_obj.copy(copy_image)
+
+        img_url = (
+            f"https://{aws_s3['BUCKET_NAME']}.s3.ap-northeast-2.amazonaws.com/{new_key}"
+        )
+        # gallary db에 저장
         new_gallary = Gallary(
             car_id=car_id,
-            car_url=car_url,
+            car_url=img_url,
             similarity=similarity,
             nickname=nickname,
             password=pw,
         )
         db.session.add(new_gallary)
+
+        # ai_result db에 업로드되었다고 수정
+        ai_db.is_upload = True
+        ai_db.most_similar_car_url = img_url
+        db.session.add(ai_db)
+
         db.session.commit()
+
         return "Created"
     except Exception as e:
         print(e)
