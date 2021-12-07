4e65e782b33d3295b159f9df1abc701a4e857209
nahyunn <nhkim0711@gmail.com>
2021-12-07 20:29:24 +0900

    fix: 비동기 오류 수정
    Former-commit-id: 608e1a225b870139cd0468c457b95ff389d7c70e

--- frontend/src/components/search/SelectedConditions.jsx
+++ frontend/src/components/search/SelectedConditions.jsx
@@ -12,18 +12,25 @@ const style = {
 
 const SelectedConditions = () => {
   const recoilState = useRecoilValue(eachConditionSelector);
-  const conditions = recoilState.filter((item) => item !== '전체~' && item.length);
-  const chipList = conditions.map((condition, idx) => {
-    return (
-      <Chip
-        key={`${condition}-${idx}`}
-        style={style}
-        label={condition}
-        variant="outlined"
-        color="primary"
-      />
-    );
-  });
+
+  const getConditions = () => {
+    if (recoilState) {
+      console.log(recoilState);
+      const conditions = recoilState.filter((item) => item !== '전체~' && item.length);
+      const chipList = conditions.map((condition, idx) => {
+        return (
+          <Chip
+            key={`${condition}-${idx}`}
+            style={style}
+            label={condition}
+            variant="outlined"
+            color="primary"
+          />
+        );
+      });
+      return chipList;
+    }
+  };
 
   return (
     <MyAccordion defaultExpanded style={{ gridColumn: '1 / 3' }}>
@@ -31,7 +38,11 @@ const SelectedConditions = () => {
         <span style={{ gridColumn: '1 / 3' }}>선택한 조건이 맞나요?</span>
       </MyAccordionSummary>
       <AccordionDetails style={{ padding: '0.5rem', paddingTop: 0 }}>
-        {chipList.length ? chipList : <Message>현재 선택한 조건이 없습니다</Message>}
+        {recoilState && getConditions().length ? (
+          getConditions()
+        ) : (
+          <Message>현재 선택한 조건이 없습니다</Message>
+        )}
       </AccordionDetails>
     </MyAccordion>
   );
