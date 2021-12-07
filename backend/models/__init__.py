2da3718d19d2b741e1ace0723a4e7eacf8847f00
redhi <redji81@naver.com>
2021-12-07 21:32:13 +0900

    [#119]fix: Ai_result 테이블 수정

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,9 +1,9 @@
 from db_connect import db
 
+from models.ai_result import Ai_Result
 from models.car import Car
 from models.car_color import CarColor
 from models.car_int import CarInt
 from models.gallary import Gallary
 from models.mbti_result import MbtiResult
-from models.temporary_ai_car import Temporary_Ai_Car
 from models.worldcup import WorldCup
