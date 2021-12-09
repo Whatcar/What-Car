b8b7676a542a90270bdd859188be567dc26f7cf0
[2기-1반] 최연주 <jujubee6323@gmail.com>
2021-12-09 19:21:18 +0900

    fix: 팀원 미모지 교체
    Former-commit-id: afbe86a7375fd4bce7670cda24783563adf60057

--- frontend/src/routes/Team.jsx
+++ frontend/src/routes/Team.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { MainTitle, SubTitle, Desc } from '../css/mainStyles';
 import styled from 'styled-components';
 import yeonju from '../img/team/yeonju.jpg';
+import nahyun from '../img/team/nahyun.png';
+import jaehyun from '../img/team/jaehyun.png';
+import jeonggyu from '../img/team/jeonggyu.jpg';
+import minji from '../img/team/minji.png';
+import seungwook from '../img/team/seungwook.jpg';
 import Member from '../components/team/member';
 import TeamImg from '../img/team/teamImg.png';
 import Layout from '../components/Layout';
@@ -12,42 +17,42 @@ export default function Team() {
       <TeamWrapper>
         <MainTitle>머선러닝29</MainTitle>
         <TeamIntro>
-          <img src={TeamImg} width="50%" alt="머선러닝29" />
+          <img src={TeamImg} width="40%" alt="머선러닝29" />
           <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
             <Desc>안녕하세요👋 저희는 머선러닝29팀입니다.</Desc>
-            <Desc>열심히 뚝딱뚝딱 만드는 중입니다 🔨</Desc>
+            <Desc>저희 팀원 모두 열심히 만든 서비스니 잘 이용해주세요!</Desc>
           </div>
         </TeamIntro>
 
         <MainTitle top={5}>팀원 소개</MainTitle>
         <Members>
           <Member
-            image={yeonju}
+            image={nahyun}
             name="김나현"
             part="프론트엔드"
             intro="아이디어 내고 뚝딱뚝딱 만드는 걸 좋아합니다!"
           />
           <Member
-            image={yeonju}
+            image={minji}
             name="김민지"
             part="백엔드 데이터분석"
             intro="안녕하십시오! 변화와 실패를 두려워하지 않는 백엔드 개발자로 진화 중인 김민지입니다."
           />
           <Member
-            image={yeonju}
+            image={jaehyun}
             name="김재현"
             part="프론트엔드"
             intro="부족하지만 열심히 하겠습니다!"
           />
 
           <Member
-            image={yeonju}
+            image={seungwook}
             name="백승욱"
             part="팀장 백엔드 데이터분석"
             intro="해에위이요옹~~"
           />
           <Member
-            image={yeonju}
+            image={jeonggyu}
             name="이정규"
             part="백엔드 AI 데이터분석"
             intro="점점 강해지는 주니어 개발자 이정규입니다~"
